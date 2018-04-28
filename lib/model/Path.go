789ca3879dd012f4fc43c413ad7eee43509b6bbe
vahriin <systemd219@gmail.com>
2018-04-28 19:04:21 +0300

    add method for path

--- lib/model/Path.go
+++ lib/model/Path.go
@@ -1,6 +1,20 @@
 package model
 
+import (
+	"github.com/vahriin/BigGraph/lib/coordinates"
+)
+
 type Path struct {
 	Points []uint64
 	Len    float64
+}
+
+func (p Path) Coordinates(al AdjList) []coordinates.EuclidCoords {
+	coords := make([]coordinates.EuclidCoords, len(p.Points))
+
+	for i, pointID := range p.Points {
+		coords[i] = al.Nodes[pointID].Euclid
+	}
+
+	return coords
 }
