6ac2c17d40d452aed2419ede24e6bb07af7da54b
vahriin <systemd219@gmail.com>
2018-04-30 22:17:27 +0300

    New PathMatrix

--- Task3/aco/PathMatrix.go
+++ Task3/aco/PathMatrix.go
@@ -24,13 +24,13 @@ func NewPathMatrix(start uint64, travelPoints map[uint64]struct{}) PathMatrix {
 		matrixIndex++
 	}
 
-	pm.pathways = make([][]model.Path, len(pm.cities))
-	for i := 0; i < len(pm.cities); i++ {
-		pm.pathways[i] = make([]model.Path, len(pm.cities))
+	pm.pathways = make([][]model.Path, len(pm.cities)-1)
+	for i := 0; i < len(pm.cities)-1; i++ {
+		pm.pathways[i] = make([]model.Path, len(pm.cities)-1-i)
 	}
-	pm.pheromones = make([][]float64, len(pm.cities))
-	for i := 0; i < len(pm.cities); i++ {
-		pm.pheromones[i] = make([]float64, len(pm.cities))
+	pm.pheromones = make([][]float64, len(pm.cities)-1)
+	for i := 0; i < len(pm.cities)-1; i++ {
+		pm.pheromones[i] = make([]float64, len(pm.cities)-1-i)
 	}
 
 	return pm
@@ -39,38 +39,89 @@ func NewPathMatrix(start uint64, travelPoints map[uint64]struct{}) PathMatrix {
 func (pm PathMatrix) Pheromone(i, j uint64) float64 {
 	pm.mutex.RLock()
 	defer pm.mutex.RUnlock()
-	return pm.pheromones[pm.cities[i]][pm.cities[j]]
+
+	if i == j {
+		return 0
+	}
+
+	iN, jN := pm.normalIndexes(i, j)
+	return pm.pheromones[iN][jN]
 }
 
 func (pm *PathMatrix) UpdatePheromone(i, j uint64, value float64) {
 	pm.mutex.Lock()
 	defer pm.mutex.Unlock()
-	pm.pheromones[pm.cities[i]][pm.cities[j]] += value
-	pm.pheromones[pm.cities[j]][pm.cities[i]] += value
+
+	if i == j {
+		return
+	}
+
+	iN, jN := pm.normalIndexes(i, j)
+
+	pm.pheromones[iN][jN] += value
 }
 
 func (pm *PathMatrix) SetPheromone(i, j uint64, value float64) {
 	pm.mutex.Lock()
 	defer pm.mutex.Unlock()
-	pm.pheromones[pm.cities[i]][pm.cities[j]] = value
-	pm.pheromones[pm.cities[j]][pm.cities[i]] = value
+
+	if i == j {
+		return
+	}
+
+	iN, jN := pm.normalIndexes(i, j)
+
+	pm.pheromones[iN][jN] = value
+
 }
 
 func (pm PathMatrix) Distance(i, j uint64) float64 {
 	pm.mutex.RLock()
 	defer pm.mutex.RUnlock()
-	return pm.pathways[pm.cities[i]][pm.cities[j]].Len
+
+	if i == j {
+		return 0
+	}
+
+	iN, jN := pm.normalIndexes(i, j)
+
+	return pm.pathways[iN][jN].Len
 }
 
 func (pm PathMatrix) Path(i, j uint64) model.Path {
 	pm.mutex.RLock()
 	defer pm.mutex.RUnlock()
-	return pm.pathways[pm.cities[i]][pm.cities[j]]
+	if i == j {
+		rp := model.Path{
+			Len:    0,
+			Points: make([]uint64, 1),
+		}
+		rp.Points[0] = i
+		return rp
+	}
+
+	iN, jN := pm.normalIndexes(i, j)
+
+	return pm.pathways[iN][jN]
 }
 
 func (pm *PathMatrix) SetPath(i, j uint64, path model.Path) {
 	pm.mutex.Lock()
 	defer pm.mutex.Unlock()
-	pm.pathways[pm.cities[i]][pm.cities[j]] = path
-	pm.pathways[pm.cities[j]][pm.cities[i]] = path
+
+	if i == j {
+		return
+	}
+
+	iN, jN := pm.normalIndexes(i, j)
+
+	pm.pathways[iN][jN] = path
+}
+
+func (pm PathMatrix) normalIndexes(i, j uint64) (iN, jN int) {
+	if pm.cities[j] < pm.cities[i] {
+		i, j = j, i
+	}
+
+	return pm.cities[i], pm.cities[j] - pm.cities[i] - 1
 }
